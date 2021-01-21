db1e8e1ece6e101d3c3737e12e710728fd7fafa2
Mr.Hatter <IIIeX89@gmail.com>
2021-01-21 23:13:33 +0300

    [FIX] Remove horizontal scroll

--- src/components/Wait.vue
+++ src/components/Wait.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="loading">
+  <b-container>
     <b-row class="justify-content-md-center">
       <b-col col md="6" class="my-4">
         <b-alert variant="success" align="center" show>
@@ -7,5 +7,5 @@
         </b-alert>
       </b-col>
     </b-row>
-  </div>
+  </b-container>
 </template>
